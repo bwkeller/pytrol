817960c03ebb2af8ef8f6a33719b4a01e59b914f
Ben Keller <malzraa@gmail.com>
2014-01-28 15:34:13 -0500

    Added parallel process method arguments

--- pytrol.py
+++ pytrol.py
@@ -6,36 +6,42 @@ import pynbody as pyn
 import multiprocessing
 
 def gasdir_iter(directory):
-    """
-    Return an iterator that spits out pynbody SimSnaps for each output.
-    """
-    dirglob = glob.glob(directory+'/*.[0-9]???[0-9]')
-    dirglob.sort()
-    pynload = lambda x: pyn.load(x)
-    return itertools.imap(pynload, dirglob.__iter__())
-    #return itertools.imap(pynload, dirglob.__iter__())
-
-def t_Myr(sim):
-    return sim.properties['time'].in_units('Myr')
-
-def time_sequence(function, directory):
-    """
-    Take a function that returns a single value and return a tuple containing ([times], [function @ times])
-    """
-    seqfunc = lambda x: (t_Myr(x), function(x))
-    tmplist = []
-    [tmplist.append(i) for i in itertools.imap(seqfunc, gasdir_iter(directory))]
-    tmplist = np.array(tmplist)
-    return (tmplist[:,0], tmplist[:,1])
-
-def parallel_process(function, directory, poolsize=64):
 	"""
-	Take a function with a single argument (a pynbody-loadable snapshot filename), and returns a list of that function run
-	on each snapshot in directory. Uses the multiprocessing library to load and process the files in parallel.
+	Return an iterator that spits out pynbody SimSnaps for each output.
 	"""
 	dirglob = glob.glob(directory+'/*.[0-9]???[0-9]')
 	dirglob.sort()
-	pool = multiprocessing.Pool(processes=poolsize)
+	pynload = lambda x: pyn.load(x)
+	return itertools.imap(pynload, dirglob.__iter__())
+	#return itertools.imap(pynload, dirglob.__iter__())
+
+def t_Myr(sim):
+	return sim.properties['time'].in_units('Myr')
+
+def time_sequence(function, directory):
+	"""
+	Take a function that returns a single value and return a tuple containing ([times], [function @ times])
+	"""
+	seqfunc = lambda x: (t_Myr(x), function(x))
 	tmplist = []
-	results = []
-	return np.array(pool.map(function, dirglob))
+	[tmplist.append(i) for i in itertools.imap(seqfunc, gasdir_iter(directory))]
+	tmplist = np.array(tmplist)
+	return (tmplist[:,0], tmplist[:,1])
+
+def parallel_process(function, directory, poolsize=64, pool=None):
+	"""
+	Take a function with a single argument (a pynbody-loadable snapshot
+	filename), and returns a list of that function run on each snapshot in
+	directory. Uses the multiprocessing library to load and process the files
+	in parallel.  You may want to pass a pool you've already created if you
+	want to run many instances and are running into a limited number of
+	processes. (this is also a reason you might want to pass a poolsize less
+	than 64.
+	"""
+	dirglob = glob.glob(directory+'/*.[0-9]???[0-9]')
+	dirglob.sort()
+	if pool==None:
+		pool = multiprocessing.Pool(processes=poolsize)
+	results = np.array(pool.map(function, dirglob))
+	return results
+
