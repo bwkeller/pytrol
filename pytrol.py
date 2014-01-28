f0c82675fcd0aa9d8219d7a8fc68fd7fa33fb8b5
Ben Keller <malzraa@gmail.com>
2014-01-28 11:29:48 -0500

    Removed some methods that are mostly handled by base numpy methods

--- pytrol.py
+++ pytrol.py
@@ -27,18 +27,3 @@ def time_sequence(function, directory):
     tmplist = np.array(tmplist)
     return (tmplist[:,0], tmplist[:,1])
 
-def write_time_sequence(function, directory, fname):
-    """
-    Take a function that returns a single value and write out a sequence of (time, function @ time) to fname
-    """
-    outfile = open(fname, 'w') 
-    seqfunc = lambda x: (t_Myr(x), function(x))
-    tmplist = []
-    [outfile.write('%5.4e %5.4e\n' % i) for i in itertools.imap(seqfunc, gasdir_iter(directory))]
-    outfile.close()
-
-def read_time_sequence(fname):
-    infile = open(fname).readlines()
-    time = [float(i.split()[0]) for i in infile]
-    data = [float(i.split()[1]) for i in infile]
-    return (time,data)
